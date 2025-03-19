cc461da697599a1dc6ae2f8308ba83cba86676a8
Emmanuel Evance <mtuchidev@gmail.com>
2025-03-19 11:25:31 +0300

    replace util.throwError with new Error

--- workflows/wf1/3-alert-admin.js
+++ workflows/wf1/3-alert-admin.js
@@ -1,6 +1,19 @@
 fn(state => {
-  util.throwError('DUPLICATE_PATIENT_NUMBERS', {
-    description: 'Found TIEs with duplicate patient numbers',
-    duplicates: state.duplicatePatients,
-  });
+  const code = 'DUPLICATE_PATIENT_NUMBERS';
+  const description = `Found ${state.duplicatePatients.length} TIEs with duplicate patient numbers`;
+  const message = `${code}: ${description}`;
+  const patientNumbers = state.duplicatePatients.map(
+    patient =>
+      patient.attributes.find(attr => attr.code === 'patient_number').value
+  );
+
+  const details = {
+    code,
+    description,
+    duplicatePatientNumbers: patientNumbers,
+  };
+  const e = new Error(message);
+  e.details = details;
+  console.error(e.details);
+  throw e;
 });
