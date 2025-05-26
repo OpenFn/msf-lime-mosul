91648bfa8286ea57a37e97d1d5d9c6847a031b01
Emmanuel Evance <mtuchidev@gmail.com>
2025-05-26 11:57:49 +0300

    filtered encounter date sorting

--- workflows/wf2/3-get-encounters.js
+++ workflows/wf2/3-get-encounters.js
@@ -91,7 +91,7 @@ const v2FormUuids = [
 //     return state;
 //   })
 // );
-get(`encounter?patient=0e3e3d1f-7819-406b-8b39-c45c89dd35dc&v=full`).then(
+get(`encounter?patient=354a512c-6bdf-4df8-abab-a2da7424ce3a&v=full`).then(
   state => {
     state.allEncounters ??= [];
     state.allEncounters.push(
@@ -100,12 +100,19 @@ get(`encounter?patient=0e3e3d1f-7819-406b-8b39-c45c89dd35dc&v=full`).then(
 
     const patientUuid = state.references.at(-1);
     const filteredEncounters = state.formUuids.map(formUuid =>
-      state.data.results.filter(
-        // TODO: Check with AK for the filter date
-        // e => e.encounterDatetime >= state.cursor && e?.form?.uuid === formUuid
-        e =>
-          e.auditInfo.dateCreated >= state.cursor && e?.form?.uuid === formUuid
-      )
+      state.data.results
+        .filter(
+          // TODO: Check with AK for the filter date
+          // e => e.encounterDatetime >= state.cursor && e?.form?.uuid === formUuid
+          e =>
+            e.auditInfo.dateCreated >= state.cursor &&
+            e?.form?.uuid === formUuid
+        )
+        .sort(
+          (a, b) =>
+            new Date(b.auditInfo.dateCreated) -
+            new Date(a.auditInfo.dateCreated)
+        )
     );
 
     const encounters = filteredEncounters.map(e => e[0]).filter(e => e);
