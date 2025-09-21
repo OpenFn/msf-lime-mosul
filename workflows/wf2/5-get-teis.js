ff6ac66546a3d5f81001268e4b4903e4a5c3877b
Emmanuel Evance <mtuchidev@gmail.com>
2025-09-21 23:24:54 +0300

    code formating

--- workflows/wf2/5-get-teis.js
+++ workflows/wf2/5-get-teis.js
@@ -12,27 +12,31 @@ fn(state => {
   }, {});
 
   return state;
-})
+});
 
-each(state => Object.keys(state.encountersByPatient), get('tracker/trackedEntities', {
-  orgUnit: $.orgUnit,
-  program: $.program,
-  filter: [`AYbfTPYMNJH:Eq:${$.data}`]
-}).then(state => {
-  const patientUid = state.references.at(-1)
+each(
+  state => Object.keys(state.encountersByPatient),
+  get('tracker/trackedEntities', {
+    orgUnit: $.orgUnit,
+    program: $.program,
+    filter: [`AYbfTPYMNJH:Eq:${$.data}`],
+  }).then(state => {
+    const patientUid = state.references.at(-1);
 
-  const tei = state.data?.instances?.[0];
-  if (tei?.trackedEntity) {
-    console.log('Parent TEI found:', tei.trackedEntity)
-    state.parentTeis ??= {};
-    state.parentTeis[patientUid] = tei;
-  } else {
-    console.log('Parent TEI Not Found for Patient:', patientUid)
-    state.missingParentTeis ??= {}
-    state.missingParentTeis[patientUid] = state.encountersByPatient[patientUid]
-  }
-  return state
-}))
+    const tei = state.data?.instances?.[0];
+    if (tei?.trackedEntity) {
+      console.log('Parent TEI found:', tei.trackedEntity);
+      state.parentTeis ??= {};
+      state.parentTeis[patientUid] = tei;
+    } else {
+      console.log('Parent TEI Not Found for Patient:', patientUid);
+      state.missingParentTeis ??= {};
+      state.missingParentTeis[patientUid] =
+        state.encountersByPatient[patientUid];
+    }
+    return state;
+  })
+);
 
 each(
   $.encounters,
@@ -54,22 +58,25 @@ each(
         enrollment: enrollments?.[0]?.enrollment,
       };
     } else {
-      state.teisToCreate ??= {}
-      const { attributes, trackedEntityType } = state.parentTeis[encounter.patient.uuid]
-      const program = state.formMaps[encounter.form.uuid].programId
-      const orgUnit = state.formMaps[encounter.form.uuid].orgUnit
+      state.teisToCreate ??= {};
+      const { attributes, trackedEntityType } =
+        state.parentTeis[encounter.patient.uuid];
+      const program = state.formMaps[encounter.form.uuid].programId;
+      const orgUnit = state.formMaps[encounter.form.uuid].orgUnit;
 
       state.teisToCreate[encounter.patient.uuid] = {
         trackedEntityType,
-        enrollments: [{
-          orgUnit,
-          program,
-          enrollmentDate: new Date().toISOString().split('T')[0],
-        }],
+        enrollments: [
+          {
+            orgUnit,
+            program,
+            enrollmentDate: new Date().toISOString().split('T')[0],
+          },
+        ],
         attributes,
         orgUnit,
-        program
-      }
+        program,
+      };
     }
 
     await delay(2000);
@@ -77,60 +84,70 @@ each(
   })
 );
 
-each(state => {
-  return state?.teisToCreate ? Object.entries(state?.teisToCreate) : []
-}, create('trackedEntityInstances', state => {
-  const payload = state.data[1]
-  return payload
-}).then(state => {
-  const [patient, payload] = state.references.at(-1)
-  const trackedEntity = state.data?.response?.importSummaries[0]?.reference
-  state.childTeis ??= {}
-  state.createdTeis ??= []
-  state.createdTeis.push(trackedEntity)
-  state.childTeis[patient] = { trackedEntity }
-  return state
-}))
+each(
+  state => {
+    return state?.teisToCreate ? Object.entries(state?.teisToCreate) : [];
+  },
+  create('trackedEntityInstances', state => {
+    const payload = state.data[1];
+    return payload;
+  }).then(state => {
+    const [patient, payload] = state.references.at(-1);
+    const trackedEntity = state.data?.response?.importSummaries[0]?.reference;
+    state.childTeis ??= {};
+    state.createdTeis ??= [];
+    state.createdTeis.push(trackedEntity);
+    state.childTeis[patient] = { trackedEntity };
+    return state;
+  })
+);
 
-each($?.createdTeis || [], get(`tracker/trackedEntities/${$.data}`, { fields: 'attributes[*],enrollments,trackedEntity' }).then(state => {
-  const { trackedEntity, enrollments, attributes } = state.data || {};
-  console.log(state.data)
-  const patientUuid = attributes.find(a => a.attribute === 'AYbfTPYMNJH').value
+each(
+  $?.createdTeis || [],
+  get(`tracker/trackedEntities/${$.data}`, {
+    fields: 'attributes[*],enrollments,trackedEntity',
+  }).then(state => {
+    const { trackedEntity, enrollments, attributes } = state.data || {};
+    console.log(state.data);
+    const patientUuid = attributes.find(
+      a => a.attribute === 'AYbfTPYMNJH'
+    ).value;
 
-  console.log('Fetched Teis', state.data)
-  state.childTeis ??= {};
-  state.childTeis[patientUuid] = {
-    trackedEntity,
-    events: enrollments?.[0]?.events,
-    enrollment: enrollments?.[0]?.enrollment,
-  };
+    console.log('Fetched Teis', state.data);
+    state.childTeis ??= {};
+    state.childTeis[patientUuid] = {
+      trackedEntity,
+      events: enrollments?.[0]?.events,
+      enrollment: enrollments?.[0]?.enrollment,
+    };
 
-  return state
-}))
-
+    return state;
+  })
+);
 
 fnIf($.childTeis && $.parentTeis, state => {
-  const { childTeis, parentTeis } = state
-  state.relationshipsMapping = Object.keys(childTeis).map(uuid => {
-    const childTei = childTeis[uuid].trackedEntity
-    const parentTei = parentTeis[uuid].trackedEntity
+  const { childTeis, parentTeis } = state;
+  state.relationshipsMapping = Object.keys(childTeis)
+    .map(uuid => {
+      const childTei = childTeis[uuid].trackedEntity;
+      const parentTei = parentTeis[uuid].trackedEntity;
 
-    if (childTei != parentTei) {
-      return {
-        "from": {
-          "trackedEntityInstance": {
-            "trackedEntityInstance": parentTei
-          }
-        },
-        "to": {
-          "trackedEntityInstance": {
-            "trackedEntityInstance": childTei
-          }
-        },
-        "relationshipType": "cJJTZ51EK24"
+      if (childTei != parentTei) {
+        return {
+          from: {
+            trackedEntityInstance: {
+              trackedEntityInstance: parentTei,
+            },
+          },
+          to: {
+            trackedEntityInstance: {
+              trackedEntityInstance: childTei,
+            },
+          },
+          relationshipType: 'cJJTZ51EK24',
+        };
       }
-    }
-
-  }).filter(Boolean)
-  return state
-})
+    })
+    .filter(Boolean);
+  return state;
+});
