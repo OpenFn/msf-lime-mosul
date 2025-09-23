a1858f3dfc8ff3900fbb35adfcdaec3cc0442a06
Emmanuel Evance <mtuchidev@gmail.com>
2025-09-23 18:57:56 +0300

    add relationship mapping

--- workflows/wf2/teis-relationships.js
+++ workflows/wf2/teis-relationships.js
@@ -1,13 +1,38 @@
+fn((state) => {
+  state.relationshipsMapping = Object.keys(state.childTeis)
+    .map((uuid) => {
+      const childTei = state.childTeis[uuid].trackedEntity;
+      const parentTei = state.parentTeis[uuid].trackedEntity;
+
+      if (childTei != parentTei) {
+        return {
+          from: {
+            trackedEntityInstance: {
+              trackedEntityInstance: parentTei,
+            },
+          },
+          to: {
+            trackedEntityInstance: {
+              trackedEntityInstance: childTei,
+            },
+          },
+          relationshipType: "cJJTZ51EK24", //TODO: Need to change this hardcoded id, because it will be different for d/t programs
+        };
+      }
+    })
+    .filter(Boolean);
+  return state;
+});
 // Check if relationship exist
 each(
   $.relationshipsMapping,
-  get('tracker/relationships', {
+  get("tracker/relationships", {
     trackedEntity: $.data.from.trackedEntityInstance.trackedEntityInstance,
-  }).then(state => {
+  }).then((state) => {
     const relationship = state.references.at(-1);
     const toTei = relationship.to.trackedEntityInstance.trackedEntityInstance;
     const hasRelationship = state.data.instances.find(
-      r => r.to.trackedEntity.trackedEntity === toTei
+      (r) => r.to.trackedEntity.trackedEntity === toTei
     );
     state.relationshipsToCreate ??= [];
     if (!hasRelationship) {
@@ -18,4 +43,4 @@ each(
 );
 
 // Creating relationship between parent and child tei
-each($.relationshipsToCreate, create('relationships', $.data));
+each($.relationshipsToCreate, create("relationships", $.data));
