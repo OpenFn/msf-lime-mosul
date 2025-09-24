65cb240b473b56166cc79f0383da980516c4a2b9
Emmanuel Evance <mtuchidev@gmail.com>
2025-09-24 11:46:25 +0300

    wip: wf3 setup

---
+++ workflows/wf3/get-patients.js
@@ -0,0 +1,36 @@
+// Enable test mode
+const testMode = false;
+
+cursor($.lastRunDateTime || $.manualCursor || '2025-03-20T06:01:24.000Z');
+
+cursor('today', {
+  key: 'lastRunDateTime',
+  format: c => dateFns.format(new Date(c), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"),
+});
+
+searchPatient({ q: 'IQ', v: 'full', includeVoide: true, limit: '100' });
+
+fn(state => {
+  const { cursor, data, lastRunDateTime } = state;
+  console.log('Filtering patients since cursor:', cursor);
+
+  const patients = data.results.filter(({ auditInfo }) => {
+    const lastModified = auditInfo?.dateChanged || auditInfo?.dateCreated;
+    return lastModified > cursor;
+  });
+  console.log('# of patients to sync to dhis2 ::', patients.length);
+  console.log(
+    'uuids of patients to sync to dhis2 ::',
+    patients.map(p => p.uuid)
+  );
+
+  return { cursor, lastRunDateTime, patients, testMode };
+});
+
+// get patient - log number of patient that are fetched
+
+// get collections neccessary for patient mapping
+
+// map patient here - this will be passed down to next step
+
+// delete the initial patient object and only pass on the mapped one
