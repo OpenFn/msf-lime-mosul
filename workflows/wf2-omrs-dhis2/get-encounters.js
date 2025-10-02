eb342d09cc9a8fa106bf05b3a4e9c4e1223b478e
Emmanuel Evance <mtuchidev@gmail.com>
2025-10-02 10:56:36 +0300

    temporary uncomment fetch all encounters

--- workflows/wf2-omrs-dhis2/get-encounters.js
+++ workflows/wf2-omrs-dhis2/get-encounters.js
@@ -39,49 +39,50 @@ function removeNulls(data) {
 //     '/ws/rest/v1/encounter?patient=0e3e3d1f-7819-406b-8b39-c45c89dd35dc&v=full'
 //   )
 //   .then(({ data }) => ({ data }));
+
 // Fetch all encounters
 
-http
-  .get("/ws/fhir2/R4/Encounter", {
-    query: { _count: 100, _lastUpdated: `ge${$.cursor}` },
-  })
-  .then((state) => {
-    const { link, total } = state.data;
-    state.nextUrl = link
-      .find((l) => l.relation === "next")
-      ?.url.replace(/(_count=)\d+/, `$1${total}`)
-      .split("/openmrs")[1];
+// http
+//   .get("/ws/fhir2/R4/Encounter", {
+//     query: { _count: 100, _lastUpdated: `ge${$.cursor}` },
+//   })
+//   .then((state) => {
+//     const { link, total } = state.data;
+//     state.nextUrl = link
+//       .find((l) => l.relation === "next")
+//       ?.url.replace(/(_count=)\d+/, `$1${total}`)
+//       .split("/openmrs")[1];
 
-    state.allResponse = state.data;
-    return state;
-  });
+//     state.allResponse = state.data;
+//     return state;
+//   });
 
-fnIf(
-  $.nextUrl,
-  http.get($.nextUrl).then((state) => {
-    console.log(`Fetched ${state.data.entry.length} remaining encounters`);
-    delete state.allResponse.link;
-    state.allResponse.entry.push(...state.data.entry);
-    return state;
-  })
-);
+// fnIf(
+//   $.nextUrl,
+//   http.get($.nextUrl).then((state) => {
+//     console.log(`Fetched ${state.data.entry.length} remaining encounters`);
+//     delete state.allResponse.link;
+//     state.allResponse.entry.push(...state.data.entry);
+//     return state;
+//   })
+// );
 
-fn((state) => {
-  console.log(
-    "Total # of encounters fetched: ",
-    state.allResponse?.entry?.length
-  );
-  // state.encounterUuids = state.allResponse?.entry?.map(p => p.resource.id);
-  state.patientUuids = [
-    ...new Set(
-      state.allResponse?.entry?.map((p) =>
-        p.resource.subject.reference.replace("Patient/", "")
-      )
-    ),
-  ];
+// fn((state) => {
+//   console.log(
+//     "Total # of encounters fetched: ",
+//     state.allResponse?.entry?.length
+//   );
+//   // state.encounterUuids = state.allResponse?.entry?.map(p => p.resource.id);
+//   state.patientUuids = [
+//     ...new Set(
+//       state.allResponse?.entry?.map((p) =>
+//         p.resource.subject.reference.replace("Patient/", "")
+//       )
+//     ),
+//   ];
 
-  return state;
-});
+//   return state;
+// });
 
 // Fetch patient encounters
 each(
@@ -193,39 +194,40 @@ fn((state) => {
   } else {
     console.log("No encounters found for cursor: ", next.cursor);
   }
-  next.allEncounters = next.allEncounters?.sort(
-          (a, b) =>
-            new Date(b.auditInfo.dateCreated) -
-            new Date(a.auditInfo.dateCreated)
-        ).map((encounter) => {
-    const { uuid, patient, obs, form, encounterDatetime } = removeLinks(
-      removeNulls(encounter)
-    );
+  next.allEncounters = next.allEncounters
+    ?.sort(
+      (a, b) =>
+        new Date(b.auditInfo.dateCreated) - new Date(a.auditInfo.dateCreated)
+    )
+    .map((encounter) => {
+      const { uuid, patient, obs, form, encounterDatetime } = removeLinks(
+        removeNulls(encounter)
+      );
 
-    return {
-      uuid,
-      patient: {
-        uuid: patient.uuid,
-        display: patient.display,
-      },
-      obs: obs.map((o) => {
-        return {
-          uuid: o.uuid,
-          concept: o.concept,
-          display: o.display,
-          formFieldPath: o.formFieldPath,
-          value: o.value,
-          person: o.person,
-        };
-      }),
-      form: {
-        uuid: form.uuid,
-        display: form.display,
-        description: form.description,
-        name: form.name,
-      },
-      encounterDatetime,
-    };
-  });
+      return {
+        uuid,
+        patient: {
+          uuid: patient.uuid,
+          display: patient.display,
+        },
+        obs: obs.map((o) => {
+          return {
+            uuid: o.uuid,
+            concept: o.concept,
+            display: o.display,
+            formFieldPath: o.formFieldPath,
+            value: o.value,
+            person: o.person,
+          };
+        }),
+        form: {
+          uuid: form.uuid,
+          display: form.display,
+          description: form.description,
+          name: form.name,
+        },
+        encounterDatetime,
+      };
+    });
   return next;
 });
