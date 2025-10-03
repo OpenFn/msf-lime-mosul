d1d09745818f0ef49c590027d6e4e4f5988507fa
Emmanuel Evance <mtuchidev@gmail.com>
2025-10-03 11:34:36 +0300

    copied latest changes from staging

--- workflows/wf1-dhis2-omrs-migration/alert-admin-of-duplicate-teis.js
+++ workflows/wf1-dhis2-omrs-migration/alert-admin-of-duplicate-teis.js
@@ -1,10 +1,10 @@
-fn(state => {
-  const code = 'DUPLICATE_PATIENT_NUMBERS';
+fn((state) => {
+  const code = "DUPLICATE_PATIENT_NUMBERS";
   const description = `Found ${state.duplicatePatients.length} TIEs with duplicate patient numbers`;
   const message = `${code}: ${description}`;
   const patientNumbers = state.duplicatePatients.map(
-    patient =>
-      patient.attributes.find(attr => attr.code === 'patient_number').value
+    (patient) =>
+      patient.attributes.find((attr) => attr.code === "patient_number").value
   );
 
   const details = {
--- workflows/wf1-dhis2-omrs-migration/update-teis.js
+++ workflows/wf1-dhis2-omrs-migration/update-teis.js
@@ -1,10 +1,10 @@
-fn(state => {
+fn((state) => {
   if (state.newPatientUuid.length === 0) {
-    console.log('No data fetched in step prior to sync.');
+    console.log("No data fetched in step prior to sync.");
   }
 
   console.log(
-    'newPatientUuid ::',
+    "newPatientUuid ::",
     JSON.stringify(state.newPatientUuid, null, 2)
   );
   return state;
@@ -14,48 +14,50 @@ fn(state => {
 each(
   $.newPatientUuid,
   upsert(
-    'trackedEntityInstances',
+    "trackedEntityInstances",
     {
       ou: $.orgUnit,
       program: $.program,
-      filter: [`${$.dhis2PatientNumberAttributeId}:Eq:${$.data.patient_number}`],
+      filter: [
+        `${$.dhis2PatientNumberAttributeId}:Eq:${$.data.patient_number}`,
+      ],
     },
-    state => {
+    (state) => {
       const payload = {
         orgUnit: state.orgUnit,
         program: state.program,
-        trackedEntityType: 'cHlzCA2MuEF',
+        trackedEntityType: "cHlzCA2MuEF",
         attributes: [
           {
             attribute: `${state.dhis2PatientNumberAttributeId}`,
             value: `${state.data.patient_number}`,
           }, //DHIS2 patient number to use as lookup key
-          { attribute: 'AYbfTPYMNJH', value: `${state.data.uuid}` }, //OMRS patient uuid
+          { attribute: "AYbfTPYMNJH", value: `${state.data.uuid}` }, //OMRS patient uuid
           {
             attribute: `${state.openmrsAutoIdAttributeId}`,
             value: `${state.data.omrs_patient_number.identifier}`,
           }, //id generated in wf1-2 e.g., "IQ146-24-000-027"
         ],
-      }
+      };
 
-      console.log('final payload to send to dhis2:', payload)
-      return payload; 
+      console.log("final payload to send to dhis2:", payload);
+      return payload;
     }
   )
-    // {
-    //   orgUnit: $.orgUnit,
-    //   program: $.program,
-    //   trackedEntityType: 'cHlzCA2MuEF',
-    //   attributes: [
-    //     {
-    //       attribute: `${$.dhis2PatientNumberAttributeId}`,
-    //       value: `${state.data.patient_number}`,
-    //     }, //DHIS2 patient number to use as lookup key
-    //     { attribute: 'AYbfTPYMNJH', value: `${state.data.uuid}` }, //OMRS patient uuid
-    //     {
-    //       attribute: `${state.openmrsAutoIdAttributeId}`,
-    //       value: `${state.data.omrs_patient_number.identifier}`,
-    //     }, //id generated in wf1-2 e.g., "IQ146-24-000-027"
-    //   ],
-    // },
+  // {
+  //   orgUnit: $.orgUnit,
+  //   program: $.program,
+  //   trackedEntityType: 'cHlzCA2MuEF',
+  //   attributes: [
+  //     {
+  //       attribute: `${$.dhis2PatientNumberAttributeId}`,
+  //       value: `${state.data.patient_number}`,
+  //     }, //DHIS2 patient number to use as lookup key
+  //     { attribute: 'AYbfTPYMNJH', value: `${state.data.uuid}` }, //OMRS patient uuid
+  //     {
+  //       attribute: `${state.openmrsAutoIdAttributeId}`,
+  //       value: `${state.data.omrs_patient_number.identifier}`,
+  //     }, //id generated in wf1-2 e.g., "IQ146-24-000-027"
+  //   ],
+  // },
 );
