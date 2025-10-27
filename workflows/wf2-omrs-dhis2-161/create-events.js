988709d11d8e44f8c60a29942912274908837661
Emmanuel Evance <mtuchidev@gmail.com>
2025-10-27 18:05:37 +0300

    add 161

--- workflows/wf2-omrs-dhis2-161/create-events.js
+++ workflows/wf2-omrs-dhis2-161/create-events.js
@@ -1,12 +1,37 @@
+const buildTeiUrl = (baseUrl, { tei, program, orgUnit }) => {
+  return `${baseUrl}/dhis-web-tracker-capture/index.html#/dashboard?tei=${tei}&program=${program}&ou=${orgUnit}`;
+};
 // Create or update events for each encounter
 create(
   "tracker",
   {
     events: (state) => {
-      console.log(
-        "Creating events for: ",
-        JSON.stringify(state.eventsMapping, null, 2)
-      );
+      const baseUrl = state.configuration.hostUrl;
+
+      const groupedEvents = state.eventsMapping.reduce((acc, event) => {
+        const { trackedEntity, program, orgUnit } = event;
+        const key = `${trackedEntity}-${program}-${orgUnit}`;
+        if (!acc[key]) {
+          acc[key] = [];
+        }
+        acc[key].push(event);
+        return acc;
+      }, {});
+      Object.split(groupedEvents).forEach((events, key) => {
+        const [trackedEntity, program, orgUnit] = key.split("-");
+
+        const teiUrl = buildTeiUrl(baseUrl, {
+          trackedEntity,
+          program,
+          orgUnit,
+        });
+
+        console.log({ events, teiUrl });
+      });
+      // console.log(
+      //   "Creating events for: ",
+      //   JSON.stringify(state.eventsMapping, null, 2)
+      // );
       return state.eventsMapping;
     },
   },
---
+++ workflows/wf2-omrs-dhis2-161/get-parent-and-child-teis.js
@@ -0,0 +1,146 @@
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const teiByPatientUuid = (patientUuid, teis) => {
+  return teis.find((tei) => {
+    const omrsPatientUuid = tei.attributes.find(
+      ({ attribute }) => attribute === "AYbfTPYMNJH"
+    )?.value;
+
+    return omrsPatientUuid === patientUuid;
+  });
+};
+
+fn((state) => {
+  // Group encounters by patient UUID
+  state.encountersByPatient = state.encounters.reduce((acc, obj) => {
+    const key = obj.patient.uuid;
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push(obj);
+    return acc;
+  }, {});
+
+  return state;
+});
+
+get("tracker/trackedEntities", {
+  orgUnit: $.orgUnit,
+  program: $.program,
+  filter: (state) => [
+    `AYbfTPYMNJH:IN:${Object.keys(state.encountersByPatient).join(";")}`,
+  ],
+});
+
+fn((state) => {
+  state.parentTeis ??= {};
+  state.missingParentTeis ??= {};
+
+  Object.keys(state.encountersByPatient).forEach((patientUuid) => {
+    const parentTei = teiByPatientUuid(patientUuid, state.data.instances);
+    if (parentTei?.trackedEntity) {
+      console.log("Parent TEI found:", parentTei.trackedEntity);
+
+      state.parentTeis[patientUuid] = {
+        trackedEntity: parentTei.trackedEntity,
+        attributes: parentTei.attributes,
+        trackedEntityType: parentTei.trackedEntityType,
+        enrollments: parentTei.enrollments,
+      };
+    } else {
+      console.log("Parent TEI Not Found for Patient:", patientUuid);
+      state.missingParentTeis[patientUuid] =
+        state.encountersByPatient[patientUuid];
+    }
+  });
+
+  return state;
+});
+
+fn((state) => {
+  state.ouProgramEncounters = state.encounters.reduce((acc, obj) => {
+    const formUuid = obj.form.uuid;
+    const patientUuid = obj.patient.uuid;
+    const orgUnit = state.formMaps[formUuid].orgUnit;
+    const program = state.formMaps[formUuid].programId;
+    const key = `${orgUnit}-${program}`;
+    if (!acc[key]) {
+      acc[key] = {
+        orgUnit,
+        program,
+        patientUuids: [patientUuid],
+      };
+    }
+    if (!acc[key].patientUuids.includes(patientUuid)) {
+      acc[key].patientUuids.push(patientUuid);
+    }
+    return acc;
+  }, {});
+
+  return state;
+});
+
+each(
+  (state) => Object.values(state.ouProgramEncounters),
+  get("tracker/trackedEntities", (state) => {
+    const { orgUnit, program, patientUuids } = state.data;
+    return {
+      orgUnit,
+      program,
+      filter: [`AYbfTPYMNJH:IN:${patientUuids.join(";")}`],
+      fields: "*",
+    };
+  }).then(async (state) => {
+    state.childTeis ??= {};
+    state.encounters.forEach((encounter) => {
+      const patientUuid = encounter.patient.uuid;
+      const tei = teiByPatientUuid(patientUuid, state.data.instances);
+
+      if (tei?.trackedEntity) {
+        console.log("Child TEI found:", tei.trackedEntity);
+        const program = tei.programOwners.find(
+          (po) => po.trackedEntity === tei.trackedEntity
+        )?.program;
+
+        state.childTeis[patientUuid] = {
+          trackedEntity: tei.trackedEntity,
+          attributes: tei.attributes,
+          trackedEntityType: tei.trackedEntityType,
+          enrollments: tei.enrollments,
+          orgUnit: tei.orgUnit,
+          program,
+        };
+      }
+
+      if (!tei && !state.childTeis[patientUuid]) {
+        console.log("Child TEI not found for patient:", patientUuid);
+        const { attributes, trackedEntityType } =
+          state?.parentTeis[patientUuid];
+        const program = state.formMaps[encounter.form.uuid].programId;
+        const orgUnit = state.formMaps[encounter.form.uuid].orgUnit;
+
+        state.childTeis[patientUuid] = {
+          trackedEntityType,
+          enrollments: [
+            {
+              orgUnit,
+              program,
+              enrolledAt: new Date().toISOString().split("T")[0],
+              attributes: attributes.filter((attribute) =>
+                [
+                  "P4wdYGkldeG", //DHIS2 ID ==> "Patient Number"
+                ].includes(attribute.attribute)
+              ),
+            },
+          ],
+          attributes,
+          orgUnit,
+          program,
+        };
+      }
+    });
+
+    await delay(2000);
+    return state;
+  })
+);
