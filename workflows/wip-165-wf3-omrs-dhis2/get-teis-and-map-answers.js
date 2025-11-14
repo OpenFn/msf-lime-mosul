bbd06a77d101282af377c0d31b7b9cc791b101e2
Emmanuel Evance <mtuchidev@gmail.com>
2025-11-14 18:50:37 +0300

    improve fetch teis in bulk

--- workflows/wip-165-wf3-omrs-dhis2/get-teis-and-map-answers.js
+++ workflows/wip-165-wf3-omrs-dhis2/get-teis-and-map-answers.js
@@ -1,32 +1,85 @@
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-//TODO: Group the encounters by patient and then get the TEI for each patient
-each(
-  $.encounters,
-  get("tracker/trackedEntities", (state) => ({
-    orgUnit: $.orgUnit,
-    program: $.program,
-    // orgUnit: state.formMaps[state.data.form.uuid].orgUnit, //TODO: the org unit and program should be fetched from fromMap by mapping encounter.form.uuid
-    // program: state.formMaps[state.data.form.uuid].programId, //TODO: the org unit and program should be fetched from fromMap by mapping encounter.form.uuid
-    filter: [`AYbfTPYMNJH:Eq:${$.data.patient.uuid}`],
-    fields: "*,enrollments[*],enrollments[events[*]], attributes[*]",
-  })).then(async (state) => {
-    const encounter = state.references.at(-1);
-    console.log(encounter.patient.uuid, "Encounter patient uuid");
+const teiByPatientUuid = (patientUuid, teis) => {
+  return teis.find((tei) => {
+    const omrsPatientUuid = tei.attributes.find(
+      ({ attribute }) => attribute === "AYbfTPYMNJH"
+    )?.value;
 
-    const { trackedEntity, enrollments, attributes } =
-      state.data?.instances?.[0] || {};
-    if (trackedEntity && enrollments) {
-      state.TEIs ??= {};
-      state.TEIs[encounter.patient.uuid] = {
-        trackedEntity,
-        events: enrollments[0]?.events,
-        enrollment: enrollments[0]?.enrollment,
-        attributes,
+    return omrsPatientUuid === patientUuid;
+  });
+};
+
+fn((state) => {
+  state.encountersPatientUuids = [
+    ...new Set(
+      Object.values(state.encountersByVisit)
+        .flat()
+        .map((encounter) => encounter.patient.uuid)
+    ),
+  ];
+
+  return state;
+});
+
+get("tracker/trackedEntities", {
+  orgUnit: $.orgUnit,
+  program: $.program,
+  filter: (state) => [
+    `AYbfTPYMNJH:IN:${state.encountersPatientUuids.join(";")}`,
+  ],
+});
+
+fn((state) => {
+  state.TEIs ??= {};
+  state.encountersPatientUuids.forEach((patientUuid) => {
+    const tei = teiByPatientUuid(patientUuid, state.data.instances);
+    if (tei?.trackedEntity) {
+      console.log("Parent TEI found:", tei.trackedEntity);
+
+      state.TEIs[patientUuid] = {
+        trackedEntity: tei.trackedEntity,
+        attributes: tei.attributes,
+        trackedEntityType: tei.trackedEntityType,
+        enrollments: tei.enrollments,
       };
+    } else {
+      console.log("Parent TEI Not Found for Patient:", patientUuid);
+      state.missingParentTeis[patientUuid] =
+        state.encountersByPatient[patientUuid];
     }
+  });
 
-    await delay(2000);
-    return state;
-  })
-);
+  return state;
+});
+
+//TODO: Group the encounters by patient and then get the TEI for each patient
+// each(
+//   (state) => Object.values(state.encountersByVisit).flat(),
+//   get("tracker/trackedEntities", (state) => ({
+//     orgUnit: $.orgUnit,
+//     program: $.program,
+//     // orgUnit: state.formMaps[state.data.form.uuid].orgUnit, //TODO: the org unit and program should be fetched from fromMap by mapping encounter.form.uuid
+//     // program: state.formMaps[state.data.form.uuid].programId, //TODO: the org unit and program should be fetched from fromMap by mapping encounter.form.uuid
+//     filter: [`AYbfTPYMNJH:Eq:${$.data.patient.uuid}`],
+//     fields: "*,enrollments[*],enrollments[events[*]], attributes[*]",
+//   })).then(async (state) => {
+//     const encounter = state.references.at(-1);
+//     console.log(encounter.patient.uuid, "Encounter patient uuid");
+
+//     const { trackedEntity, enrollments, attributes } =
+//       state.data?.instances?.[0] || {};
+//     if (trackedEntity && enrollments) {
+//       state.TEIs ??= {};
+//       state.TEIs[encounter.patient.uuid] = {
+//         trackedEntity,
+//         events: enrollments[0]?.events,
+//         enrollment: enrollments[0]?.enrollment,
+//         attributes,
+//       };
+//     }
+
+//     await delay(2000);
+//     return state;
+//   })
+// );
