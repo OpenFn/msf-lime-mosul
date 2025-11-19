c177fbc0592de5fad78f7625bd9808e851bc4d7d
Emmanuel Evance <mtuchidev@gmail.com>
2025-11-19 11:15:08 +0300

    fetch events

--- workflows/wip-165-wf3-omrs-dhis2/get-teis-and-map-answers.js
+++ workflows/wip-165-wf3-omrs-dhis2/get-teis-and-map-answers.js
@@ -27,11 +27,8 @@ fn((state) => {
       console.log("Parent TEI found:", tei.trackedEntity);
 
       state.TEIs[patientUuid] = {
-        trackedEntity: tei.trackedEntity,
-        attributes: tei.attributes,
-        trackedEntityType: tei.trackedEntityType,
+        ...tei,
         enrollment: tei.enrollments[0]?.enrollment,
-        events: tei.enrollments[0]?.events,
       };
     } else {
       console.log("Parent TEI Not Found for Patient:", patientUuid);
@@ -40,3 +37,60 @@ fn((state) => {
 
   return state;
 });
+
+fn((state) => {
+  state.ouProgramTeis = state.latestEncountersByVisit.reduce((acc, obj) => {
+    const formUuid = obj.form.uuid;
+    // const patientUuid = obj.patient.uuid;
+    const patientNumber = obj.patient.display.split(" - ")[0];
+    const orgUnit = state.formMaps[formUuid].orgUnit;
+    const program = state.formMaps[formUuid].programId;
+    const key = `${orgUnit}-${program}`;
+    if (!acc[key]) {
+      acc[key] = {
+        orgUnit,
+        program,
+        patientNumbers: [patientNumber],
+      };
+    }
+    if (!acc[key].patientNumbers.includes(patientNumber)) {
+      acc[key].patientNumbers.push(patientNumber);
+    }
+
+    return acc;
+  }, {});
+
+  return state;
+});
+
+each(
+  (state) => Object.values(state.ouProgramTeis),
+  get("tracker/events", (state) => {
+    const { orgUnit, program, patientNumbers } = state.data;
+    return {
+      orgUnit,
+      program,
+      filter: [`Pi1zytYdq6l:IN:${patientNumbers.join(";")}`],
+      fields: "*",
+    };
+  }).then((state) => {
+    const { orgUnit, program } = state.references.at(-1);
+    state.eventsByPatient ??= {};
+    const grouped = state.data.instances.reduce((acc, event) => {
+      const patientNumber = event.dataValues.find(
+        (dv) => dv.dataElement === "Pi1zytYdq6l"
+      )?.value;
+
+      if (!acc[patientNumber]) {
+        acc[patientNumber] = [];
+      }
+      acc[patientNumber].push({
+        event: event.event,
+        occuredAt: event.occuredAt,
+      });
+      return acc;
+    }, {});
+    state.eventsByPatient[`${orgUnit}-${program}`] = grouped;
+    return state;
+  })
+);
