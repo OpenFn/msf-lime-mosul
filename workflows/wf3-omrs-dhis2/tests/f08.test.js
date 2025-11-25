19efe31741ec79c9d37dbf8c7ef62adcf63a5d6e
Emmanuel Evance <mtuchidev@gmail.com>
2025-11-25 18:06:28 +0300

    test f08 function

--- workflows/wf3-omrs-dhis2/tests/f08.test.js
+++ workflows/wf3-omrs-dhis2/tests/f08.test.js
@@ -1,21 +1,28 @@
+import assert from "node:assert";
 import { describe, it } from "node:test";
+import { readFile } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import importJob from "../../../importer.js";
-import f08Encounter from "./test-data/f08-encounter.json" with { type: "json" };
-const { f08 } = await importJob("../custom-logic-for-events.js");
 
-describe("f08", () => {
+const f8EncounterFile = new URL(
+  "./test-data/f08-encounter.json",
+  import.meta.url
+);
+const f08Encounter = JSON.parse(await readFile(f8EncounterFile, "utf8"));
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const { f8 } = await importJob(
+  path.join(__dirname, "../custom-logic-for-events.js")
+);
+
+describe("f8", () => {
   it("should return an array of data values", () => {
-    const res = f08(f08Encounter);
-
-    expect(res).toEqual([
-      {
-        dataElement: "iQio7NYSA3m",
-        value: "01/01/2022",
-      },
-      {
-        dataElement: "yprMS34o8s3",
-        value: "",
-      },
-    ]);
+    const res = f8(f08Encounter);
+    assert.equal(res.length, 2);
+    assert.deepEqual(res[0], {
+      dataElement: "iQio7NYSA3m",
+      value: "2025-11-21",
+    });
   });
 });
