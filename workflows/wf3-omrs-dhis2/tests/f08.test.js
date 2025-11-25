d38b32829e3688d82a451dd35341d77de705996f
Emmanuel Evance <mtuchidev@gmail.com>
2025-11-25 18:38:33 +0300

    test encounter without admission date

--- workflows/wf3-omrs-dhis2/tests/f08.test.js
+++ workflows/wf3-omrs-dhis2/tests/f08.test.js
@@ -6,10 +6,12 @@ import { fileURLToPath } from "node:url";
 import importJob from "../../../importer.js";
 
 const f8EncounterFile = new URL(
-  "./test-data/f08-encounter.json",
+  "./test-data/f08-encounters.json",
   import.meta.url
 );
 const f08Encounter = JSON.parse(await readFile(f8EncounterFile, "utf8"));
+const withAdmissionDate = f08Encounter[1];
+const withoutAdmissionDate = f08Encounter[0];
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const { f8 } = await importJob(
@@ -17,12 +19,17 @@ const { f8 } = await importJob(
 );
 
 describe("f8", () => {
-  it("should return an array of data values", () => {
-    const res = f8(f08Encounter);
+  it("should return an array of data values if admission date is present", () => {
+    const res = f8(withAdmissionDate);
     assert.equal(res.length, 2);
     assert.deepEqual(res[0], {
       dataElement: "iQio7NYSA3m",
-      value: "2025-11-21",
+      value: "2025-11-05",
     });
+  });
+  it("should return empty array if admission date is not present", () => {
+    const res = f8(withoutAdmissionDate);
+    assert.equal(res.length, 0);
+    assert.deepEqual(res, []);
   });
 });
