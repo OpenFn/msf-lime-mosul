e5eb7afcf6664d06ec152b3c2c4f9aed5a5e875a
Emmanuel Evance <mtuchidev@gmail.com>
2025-12-15 20:38:43 +0300

    use tracker api

--- workflows/wf1-dhis2-omrs-migration/update-teis.js
+++ workflows/wf1-dhis2-omrs-migration/update-teis.js
@@ -1,61 +1,96 @@
-fn(state => {
+function chunkArray(array, size) {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size));
+  }
+  return chunks;
+}
+
+fn((state) => {
   if (state.newPatientUuid.length === 0) {
-    console.log('No data fetched in step prior to sync.');
+    console.log("No data fetched in step prior to sync.");
   }
 
-  console.log(
-    'newPatientUuid ::',
-    JSON.stringify(state.newPatientUuid, null, 2)
-  );
+  // console.log(
+  //   'newPatientUuid ::',
+  //   JSON.stringify(state.newPatientUuid, null, 2)
+  // );
+  console.log("newPatientUuid", state.newPatientUuid.length);
+  state.patientNumbers = state.newPatientUuid.map((p) => p.patient_number);
   return state;
 });
 
-// Update TEI on DHIS2
 each(
-  $.newPatientUuid,
-  upsert(
-    'trackedEntityInstances',
-    {
-      ou: $.orgUnit,
-      program: $.program,
-      filter: [`${$.dhis2PatientNumberAttributeId}:Eq:${$.data.patient_number}`],
-    },
-    state => {
-      const payload = {
-        orgUnit: state.orgUnit,
-        program: state.program,
-        trackedEntityType: 'cHlzCA2MuEF',
-        attributes: [
-          {
-            attribute: `${state.dhis2PatientNumberAttributeId}`,
-            value: `${state.data.patient_number}`,
-          }, //DHIS2 patient number to use as lookup key
-          { attribute: 'AYbfTPYMNJH', value: `${state.data.uuid}` }, //OMRS patient uuid
-          {
-            attribute: `${state.openmrsAutoIdAttributeId}`,
-            value: `${state.data.omrs_patient_number.identifier}`,
-          }, //id generated in wf1-2 e.g., "IQ146-24-000-027"
-        ],
-      }
-
-      console.log('final payload to send to dhis2:', payload)
-      return payload;
-    }
-  )
-  // {
-  //   orgUnit: $.orgUnit,
-  //   program: $.program,
-  //   trackedEntityType: 'cHlzCA2MuEF',
-  //   attributes: [
-  //     {
-  //       attribute: `${$.dhis2PatientNumberAttributeId}`,
-  //       value: `${state.data.patient_number}`,
-  //     }, //DHIS2 patient number to use as lookup key
-  //     { attribute: 'AYbfTPYMNJH', value: `${state.data.uuid}` }, //OMRS patient uuid
-  //     {
-  //       attribute: `${state.openmrsAutoIdAttributeId}`,
-  //       value: `${state.data.omrs_patient_number.identifier}`,
-  //     }, //id generated in wf1-2 e.g., "IQ146-24-000-027"
-  //   ],
-  // },
+  (state) => chunkArray(state.patientNumbers, 100),
+  get("tracker/trackedEntities", {
+    orgUnit: $.orgUnit,
+    filter: (state) => [
+      `${state.dhis2PatientNumberAttributeId}:IN:${state.data.join(";")}`,
+    ],
+    program: $.program,
+    fields: "*",
+  }).then((state) => {
+    state.foundTrackedEntities ??= [];
+    state.foundTrackedEntities.push(...state.data.instances);
+    return state;
+  })
 );
+
+fn((state) => {
+  state.patientsMapping = state.newPatientUuid.map((p) => {
+    const trackedEntity = state.foundTrackedEntities.find((tei) => {
+      return (
+        tei.attributes.find(
+          (attribute) => attribute.attribute === "AYbfTPYMNJH"
+        )?.value === p.uuid
+      );
+    })?.trackedEntity
+    return {
+      trackedEntity,
+      orgUnit: state.orgUnit,
+      program: state.program,
+      trackedEntityType: "cHlzCA2MuEF",
+      attributes: [
+        {
+          attribute: `${state.dhis2PatientNumberAttributeId}`,
+          value: `${p.patient_number}`,
+        }, //DHIS2 patient number to use as lookup key
+        { attribute: "AYbfTPYMNJH", value: `${p.uuid}` }, //OMRS patient uuid
+        {
+          attribute: `${state.openmrsAutoIdAttributeId}`,
+          value: `${p.omrs_patient_number.identifier}`,
+        }, //id generated in wf1-2 e.g., "IQ146-24-000-027"
+      ],
+    };
+  });
+  return state;
+});
+
+// Bulk upsert
+create(
+  "tracker",
+  { trackedEntities: $.patientsMapping },
+  {
+    params: {
+      atomicMode: "ALL",
+      importStrategy: "CREATE_AND_UPDATE",
+      async: false,
+    },
+  }
+);
+fn((state) => {
+  const {
+    data,
+    response,
+    references,
+    patients,
+    patientsUpsert,
+    placeOflivingMap,
+    foundTrackedEntities,
+    identifiers,
+    patientNumbers,
+    ...next
+  } = state;
+
+  return next;
+});
