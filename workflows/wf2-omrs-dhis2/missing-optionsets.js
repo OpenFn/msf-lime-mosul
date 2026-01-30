1eeb942d845aaf3288ff307351e8a57cdbf33add
Emmanuel Evance <mtuchidev@gmail.com>
2026-01-30 12:15:55 +0300

    add mising optionsets googlesheet step

---
+++ workflows/wf2-omrs-dhis2/missing-optionsets.js
@@ -0,0 +1,9 @@
+
+batchUpdateValues({
+  spreadsheetId: '13yjqiejKFf6HlNCvgfgaKNi9Gffhq22RUTwXKUPR36k',
+  range: 'Missing OptionSets Mappings',
+  values: [
+    ['From expression', '$15', '2', '3/15/2016'],
+    ['Really now!', '$100', '1', '3/20/2016'],
+  ],
+})
