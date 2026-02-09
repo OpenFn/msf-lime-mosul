ac4065d887634280edd83c97fbb643fcbaafe60f
Emmanuel Evance <mtuchidev@gmail.com>
2026-02-09 11:09:52 +0300

    pass testMode btn steps

--- workflows/wf3-omrs-dhis2/get-patients.js
+++ workflows/wf3-omrs-dhis2/get-patients.js
@@ -134,11 +134,13 @@ fn((state) => {
     patients,
     searchPatientUuids,
     encounterPatientUuids,
+    testMode,
   } = state;
 
-  if (state.testMode) {
+  if (testMode) {
     return {
       cursor,
+      testMode,
       lastRunDateTime,
       patients,
       patientUuids: [...new Set(searchPatientUuids)],
@@ -163,5 +165,5 @@ fn((state) => {
   console.log("R4/Encounter only", onlyInR4Encounter.length);
   console.log("searchPatient() + R4/Encounter Uuids", patientUuids.length);
 
-  return { cursor, lastRunDateTime, patients, patientUuids };
+  return { cursor, lastRunDateTime, patients, patientUuids, testMode };
 });
