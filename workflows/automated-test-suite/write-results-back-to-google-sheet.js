7a4d27eb2a1465e236a8c40740b4f736b5d8e38d
Emmanuel Evance <mtuchidev@gmail.com>
2026-03-26 14:25:58 +0300

    remove justin test workflows

--- workflows/automated-test-suite/write-results-back-to-google-sheet.js
+++
@@ -1,1 +0,0 @@
-// Add operations here
--- workflows/wf3-omrs-dhis2-jfcopy/populate-outcomes-in-test-suite.js
+++
@@ -1,5 +0,0 @@
-appendValues({
-  spreadsheetId: '1--fpoThZj_LMZQW3jOij6X8kWOnE9TUx6fGNf6CYlRY',
-  range: 'Test Cases!F2',
-  values: [[JSON.stringify(state.eventsMapping)]]
-});
