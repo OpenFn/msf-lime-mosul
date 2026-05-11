19b801ae3823283aeae78823e9a8aa213600cd35
Emmanuel Evance <mtuchidev@gmail.com>
2026-05-11 20:16:23 +0300

    rename collections

--- workflows/wf0-sync-metadata/save-option-set-json-to-collections-main.js
+++ workflows/wf0-sync-metadata/save-option-set-json-to-collections-main.js
@@ -1,45 +1,45 @@
 // Check out the Job Writing Guide for help getting started:
 // https://docs.openfn.org/documentation/jobs/job-writing-guide
 // Cleanup: remove all collections data
-collections.remove('mosul-metadata-mappings-main', '*');
+collections.remove('mosul-metadata-mappings', '*');
 
-collections.set('mosul-metadata-mappings-main', 'syncedAt', $.syncedAt);
-collections.set('mosul-metadata-mappings-main', 'sourceFile', $.sourceFile);
+collections.set('mosul-metadata-mappings', 'syncedAt', $.syncedAt);
+collections.set('mosul-metadata-mappings', 'sourceFile', $.sourceFile);
 collections.set(
-  'mosul-metadata-mappings-main',
+  'mosul-metadata-mappings',
   'formMetadata',
   $.formMetadata
 );
 collections.set(
-  'mosul-metadata-mappings-main',
+  'mosul-metadata-mappings',
   'fileDateModified',
   $.fileDateModified
 );
 
 collections.set(
-  'mosul-metadata-mappings-main',
+  'mosul-metadata-mappings',
   'optionSetKey',
   $.optionSetKey
 );
 
-collections.set('mosul-metadata-mappings-main', 'formMaps', $.formMaps);
+collections.set('mosul-metadata-mappings', 'formMaps', $.formMaps);
 
 collections.set(
-  'mosul-metadata-mappings-main',
+  'mosul-metadata-mappings',
   'placeOflivingMap',
   $.placeOflivingMap
 );
 
 let itemCount = 0;
 collections.set(
-  'mosul-metadata-mappings-main',
+  'mosul-metadata-mappings',
   i => `optsMap-value-${++itemCount}`,
   $.optsMap
 );
 
 itemCount = 0;
 collections.set(
-  'mosul-metadata-mappings-main',
+  'mosul-metadata-mappings',
   i => `identifiers-value-${++itemCount}`,
   $.identifiers
 );
