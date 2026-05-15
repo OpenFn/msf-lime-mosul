0a909f89a8eb5144d1d9d776040248406d99b0ef
Emmanuel Evance <mtuchidev@gmail.com>
2026-05-15 09:24:31 +0300

    remove test workflows

--- workflows/wf2-omrs-dhis2-theory/prepare-tests.js
+++
@@ -1,4 +0,0 @@
-fn(state => {
-  const { test_input } = state.data.body[0];
-  return { data: { test_input } };
-});
--- workflows/wf3-omrs-dhis2-jfcopy/prepare-tests.js
+++
@@ -1,6 +0,0 @@
-fn(state => {
-  return {
-    ...state.data.body[0].test_input,
-    testMode: true
-  };
-});
